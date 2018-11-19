3d055dae936da367db4a87cc29e15cef6e447796
sravanongit <sravan.km@live.com>
2018-11-20 01:14:46 +0530

    perl5.pl has been appended, and concat.sh has been been created.

---
+++ concat.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+a="Sravan"
+b="Kumar"
+echo $a.$b
+a=5
+b=10
+printf $((a*b))"\n"
--- perl5.pl
+++ perl5.pl
@@ -1,8 +1,9 @@
 #!/usr/bin/perl
 use warnings;
 use strict;
-my $a=10;
+my $a=30;
 my $b=20;
 print "The smaller number is ";
 $a<$b ? print $a : print $b;
 print "\n";
+print $a<=>$b,"\n";
