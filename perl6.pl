46be509d19c777025a8d325f966ea035f4aa1fca
sravanongit <sravan.km@live.com>
2018-11-22 01:14:37 +0530

    perl6.pl file has been created.

---
+++ perl6.pl
@@ -0,0 +1,9 @@
+#!/usr/bi/perl
+use warnings;
+use strict;
+my %belongs=("Lucknow","UP","Patna","Bihar","Bhopal","MP","Ahmedabad","Gujrat","Bangalore","Karnataka","Mysore","Karnataka","Prayagraj","UP","Kashi","UP");
+print keys %belongs,"\n";
+print $belongs{"Lucknow"},"\n";
+print values %belongs,"\n";
+my @name=("Ram","Sita","Lakshman","Hanuman");
+print $name[0],"\n";
