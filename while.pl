2487ae641df23bc3c884209a9d9aed6c7122b3ba
sravanongit <sravan.km@live.com>
2018-11-27 03:06:28 +0530

    while.pl file has ben created.

---
+++ while.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+print "Enter an integer: ";
+my $input=<STDIN>;
+chomp($input);
+my $i=0;
+while($i<=$input)
+{
+	if($i%2!=0)
+	{
+		print $i," is a ODD number.","\n";
+	}
+	$i=$i+1;
+}
+print "The interpretation of this script has been completed.","\n";
