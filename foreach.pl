c2cf1faa0aa482a1ffab0eddc90bd893b7ddab34
sravanongit <sravan.km@live.com>
2018-12-03 01:13:57 +0530

    foreach.pl file has been added.

---
+++ foreach.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+print $^V,"\n";
+print "Enter the length of the array: ";
+my $length=<STDIN>;
+chomp($length);
+my @number=();
+print "\nEnter the elements of the array: \n";
+for(my $i=0;$i <= ($length-1);$i++)
+{
+	$number[$i]=<STDIN>;
+	chomp($number[$i]);
+}
+my $var;
+foreach $var (@number)
+{
+print "$var ";
+}
+print "\n";
+
