468beb78422f3d901fa4fad60cb3d1ac8746369a
sravanongit <sravan.km@live.com>
2018-12-03 01:18:48 +0530

    Folder named ankita has been deleted.

--- ankita/gcd.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main(){
-	int a,b;
-	printf("Enter 2 numbers to find GCD or HCF: ");
-	scanf("%d %d",&a,&b);
-	if(a>b)
-	{
-		for(i=1;i<=a;i++)
-			if(
--- ankita/swap.c
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-int main(){
-	int a,b;
-	printf("Enter 2 number: ");
-        sa
